b13a065d7f55e0fa784c212aca2ca6fe084d4bdf
nmikhol <nataliemikhol@gmail.com>
2019-10-09 19:50:40 -0500

    finish cosmetic score board and display game status

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 $(function () {
-
+  let playerScore = 0
+  let botScore = 0
   let playerChoice
   let botChoice
-  $("button").click((event) => {
+  $("button").not('#reset').click((event) => {
     playerChoice =  $(event.currentTarget).attr('id')
     botChoice = generateBotChoice()
     compare(botChoice, playerChoice)
-    console.log('bot choice: ' + botChoice)
-    console.log('player choice: ' + playerChoice)
+  })
+
+  $('#reset').click(() => {
+     playerScore = 0
+     botScore = 0
+     clearStatus()
+     $('#humanScore').html(playerScore)
+     $('#computerScore').html(botScore)
 
   })
 
@@ -26,25 +33,55 @@ $(function () {
 
   function compare(botChoice, playerChoice) {
     if(botChoice === playerChoice){
-      console.log("It's a tie!")
-    } else if(botChoice === "rock"){
+      clearStatus()
+      winnerText(botChoice, playerChoice, "No One")
+    }
+
+    else if(botChoice === "rock"){
       if(playerChoice === 'scissors'){
-        console.log("bot wins")
+        clearStatus()
+        winnerText(botChoice, playerChoice, "Computer")
+        $('#computerScore').html(botScore += 1)
         } else{
-          console.log("player wins") //
-        }
-    } else if(botChoice === "scissors"){
-      if(playerChoice === "paper"){
-        console.log("bot wins")
-        } else{
-          console.log("player wins") //scissors +rocks
-        }
-    } else if(botChoice === "paper"){
-      if(playerChoice === 'rock'){
-        console.log("bot wins")
-        }else{
-          console.log("player wins") //
+          clearStatus()
+          winnerText(botChoice, playerChoice, "Player")
+          $('#humanScore').html(playerScore += 1)
         }
     }
+
+    else if(botChoice === "scissors"){
+      if(playerChoice === "paper"){
+        clearStatus()
+        winnerText(botChoice, playerChoice, "Computer")
+        $('#computerScore').html(botScore += 1)
+      } else{
+          clearStatus()
+          $('#status').append("You Win!")
+          winnerText(botChoice, playerChoice, "Player")
+          $('#humanScore').html(playerScore += 1)
+        }
+    }
+
+    else if(botChoice === "paper"){
+      if(playerChoice === 'rock'){
+        clearStatus()
+        winnerText(botChoice, playerChoice, "Computer")
+        $('#computerScore').html(botScore += 1)
+      } else{
+          clearStatus()
+          winnerText(botChoice, playerChoice, "Player")
+          $('#humanScore').html(playerScore += 1)
+        }
+      }
   }
+
+  function winnerText(botChoice, playerChoice, winner){
+    $('#status').append(`You played <u>${playerChoice}</u>. The bot played <u>${botChoice}</u>. <br>${winner} Wins!`)
+  }
+
+  function clearStatus(){
+    $('#status').html('')
+  }
+
+
 })
